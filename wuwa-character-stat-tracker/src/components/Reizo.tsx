0eeb28d6043152ef25122ad51ecd1de948a2b1f3
Steven Zheng <taka23231@icloud.com>
2026-01-09 19:04:46 -0500

    uploading img

--- wuwa-character-stat-tracker/src/components/Reizo.tsx
+++ wuwa-character-stat-tracker/src/components/Reizo.tsx
@@ -1,12 +1,14 @@
 
-import { ReizoData } from "../data/ReizoData"
+import  ReizoData from "../data/ReizoData.json"
 import { Profile } from "./Profile"
+import type { CharacterData } from "../types/CharacterData";
 
+const myCharacter = ReizoData as CharacterData[];
 export const Reizo = () => {
     return (
         <div className="flex flex-col items-center bg-[ulr(/full-art/Chixia.webp)]">
             <h1 className="font-bold text-4xl text-white bg-black w-full text-center">Reizo</h1>
-            <Profile Data={ReizoData}/>         
+            <Profile Data={myCharacter}/>         
         </div>
     )
 }
